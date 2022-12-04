1d85570ad0ca4d5316cb79436f2ad4ecabc842b6
cottiera <cottiera@shaw.ca>
2022-12-03 20:35:11 -0700

    completed

--- day3.py
+++ day3.py
@@ -44,17 +44,41 @@ else:  # Part II
 
     list_of_3 = []
     badge_score = 0
+    count1 = 0
+    count2 = 0
     for line in lines:
         line = line.replace('\n', '')
+        list_of_3.append(line)
         if len(list_of_3) == 3:
+            print(list_of_3)
             for letter in list_of_3[0]:
-                if letter in list_of_3[1]:
-                    if letter in list_of_3[2]:
-                        badge_score = priorities[letter]
-                        
+                if (letter in list_of_3[1]) and (letter in list_of_3[2]):
+                    print(letter)
+                    badge_score += priorities[letter]
+                    break
             list_of_3 = []
-        else: 
-            list_of_3.append(line)
+        else:
+            continue
+            """
+            print(list_of_3)
+            for letter in list_of_3[0]:
+                count1 = 0
+                for letter_1 in list_of_3[1]:
+                    count2 = 0
+                    if letter_1 == letter:
+                        count1 += 1
+                if count1 == 1:
+                    for letter_2 in list_of_3[2]:
+                        if letter_2 == letter:
+                            print("This letter is in all three")
+                            print(letter)
+                            count2 += 1
+                    if count2 == 1:
+                        print("This letter is in all three and only once")
+                        print(letter)
+                        badge_score += priorities[letter]
+                        break
+            """
 
     print(badge_score)
 
